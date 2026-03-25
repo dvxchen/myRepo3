6511fc10e3d2a5522efd0aa30521788bc5d2045b
dvxchen <davy.chen@sap.com>
2026-03-25 21:53:05 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,16 +4,7 @@ module.exports = defineConfig({
   e2e: {
     specPattern: process.env.CYPRESS_CASENAME,
     video: false,
-    supportFile: false,
-
-    // 👇 只加这一段，忽略 WebGL 错误，不影响你原有配置
-    setupNodeEvents(on, config) {
-      on('uncaught:exception', (err, runnable) => {
-        // 忽略网页内部错误（包括 WebGL）
-        return false;
-      });
-      return config;
-    }
+    supportFile: 'cypress/support/e2e.js' // 开启支持文件
   },
   chromeWebSecurity: false,
   headless: true
